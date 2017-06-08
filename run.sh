ae0d2dcdaab7656c3d02a838d314c9dc93c3c860
Dag Viggo Lokøen <dagviggo@vimond.com>
2017-06-08 12:15:36 -0700

    Execute correct command on startup

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cp -v /config-files/* keycloak/standalone/configuration/
 
-exec /opt/jboss/docker-entrypoint.sh $@
+exec /opt/jboss/docker-entrypoint.sh -b 0.0.0.0 --server-config standalone-ha.xml
